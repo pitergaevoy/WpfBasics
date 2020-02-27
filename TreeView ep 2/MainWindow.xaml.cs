3c5dc960a83118896a7d7995104e88364763b7f5
Gaevoy Piter <pitergaevoy@hotmail.com>
2020-02-27 17:53:39 +0300

    Icon show-off test

--- TreeView ep 2/MainWindow.xaml.cs
+++ TreeView ep 2/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace WpfApp_TreeView_Learn
 {
@@ -7,9 +9,46 @@ namespace WpfApp_TreeView_Learn
     /// </summary>
     public partial class MainWindow
     {
+        #region Constructor
+        ///<summary>
+        ///DefaultConstructor
+        /// </summary>
+
+
+        
         public MainWindow()
         {
-            InitializeComponent();
+            
         }
+
+        #endregion
+
+        #region OnLoaded
+        /// <summary>
+        /// When application first opens
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        { 
+            //Get every logical drive on machine
+            foreach (var drive in Directory.GetLogicalDrives())
+            {
+                //Create a new item for it
+                var item = new TreeViewItem();
+                
+                //St the header and path
+                item.Header = drive;
+                item.Tag = drive;
+
+                //Empty Item
+                item.Items.Add(null);
+
+                //Add it ti the main tree-view
+                FolderView.Items.Add(item);
+            }
+        }
+        #endregion
     }
 }
